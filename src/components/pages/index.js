c5b1ee6c49d6e17fb36863da1ce155a7803a0243
Jorge <20190542@aloe.ulima.edu.pe>
2022-10-11 14:08:54 -0500

    1

---
+++ src/components/pages/index.js
@@ -0,0 +1,4 @@
+export {default as TwentyfirstPage} from "./TwentyfirstPage";
+export {default as ThridPage} from "./ThridPage";
+export {default as SeventhPage} from "./SeventhPage";
+export {default as NineteenthPage} from "./NineteenthPage";
